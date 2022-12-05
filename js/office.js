389f09d9e5e01a1462b8143edb3dc320a6bcf33a
WnX <vandermeulen.christophe@gmail.com>
2022-12-05 08:12:48 +0100

    update

--- js/office.js
+++ js/office.js
@@ -1,8 +1,31 @@
-const office = {};
+const office = {
+    data : []
+
+};
 
 office.init = function () {
+    //get DOM elements
+    office.tableContent = document.querySelector('#container-list table tbody')
+    console.log(office.tableContent,'office.tableContent')
+
+    office.data = office.getAll();
     console.log('Initialisation du controller office');
 }
+
+office.getAll = () => {
+    //MOCK
+
+    return [
+        {
+            id: 1,
+            name: "Bureau 1"
+        },
+        {
+            id: 2,
+            name: "Bureau 2"
+        }
+    ]
+};
 
 app.controllers.office = office;
 
